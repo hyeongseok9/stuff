41e6ecfebac8fc00310279d2c0d22f2855373319
hyeongseok nam <hyeongseok.nam@gmail.com>
2019-09-28 05:05:51 +0100

    added startup script

---
+++ RemoteCameraClient/install_libs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+sudo apt-get install libjpeg-dev libtiff-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev
+
+sudo pip3.6 install Pillow interpolate3
---
+++ RemoteCameraWebViewer/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3.6 /home/pi/git_repo/stuff/RemoteCameraWebViewer/main.py

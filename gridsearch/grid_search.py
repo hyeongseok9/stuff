df752c4200b58f9d5ea0507068669abfe1ed4400
hsnam <hsnam@whatap.io>
2020-10-01 20:31:41 +0900

    grid_search

--- gridsearch/grid_search.py
+++ gridsearch/grid_search.py
@@ -1,101 +1,45 @@
 # grid search sarima hyperparameters
-from math import sqrt
-from multiprocessing import cpu_count
-from warnings import catch_warnings
-from warnings import filterwarnings
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from sklearn.metrics import mean_squared_error
-from pandas import read_csv
-import time
+import grid_search_async
 
-
-# one-step sarima forecast
-def sarima_forecast(history, config):
-    order, sorder, trend = config
-    # define model
-    model = SARIMAX(history, order=order, seasonal_order=sorder, trend=trend, enforce_stationarity=False, enforce_invertibility=False)
-    # fit model
-    model_fit = model.fit(disp=False)
-    # make one step forecast
-    yhat = model_fit.predict(len(history), len(history))
-    return yhat[0]
-
-# root mean squared error or rmse
-def measure_rmse(actual, predicted):
-    return sqrt(mean_squared_error(actual, predicted))
-
-# split a univariate dataset into train/test sets
-def train_test_split(data, n_test):
-    return data[:-n_test], data[-n_test:]
-
-# walk-forward validation for univariate data
-def walk_forward_validation(data, n_test, cfg):
-    predictions = list()
-    # split dataset
-    train, test = train_test_split(data, n_test)
-    # seed history with training dataset
-    history = [x for x in train]
-    # step over each time-step in the test set
-    total = len(test)
-    total_process_started = time.time()
-    for i in range(total):
-        # fit model and make forecast for history
-        yhat = sarima_forecast(history, cfg)
-        # store forecast in list of predictions
-        predictions.append(yhat)
-        # add actual observation to history for the next loop
-        history.append(test[i])
-    # estimate prediction error
-    error = measure_rmse(test, predictions)
-    return error
-
-# score a model, return None on failure
-def score_model(data, n_test, cfg, debug=False):
-    result = None
-    # convert config to a key
-    key = str(cfg)
-    # show all warnings and fail on exception if debugging
-    if debug:
-        result = walk_forward_validation(data, n_test, cfg)
-    else:
-        # one failure during model validation suggests an unstable config
-        try:
-            # never show warnings when grid searching, too noisy
-            with catch_warnings():
-                filterwarnings("ignore")
-                result = walk_forward_validation(data, n_test, cfg)
-        except Exception as e:
-            print('score_model', e)
-            error = None
-    # check for an interesting result
-    if result is not None:
-        print(' > Model[%s] %.3f' % (key, result))
-    return (key, result)
-
-
-def grid_search(data, cfg, n_test):
-    score = score_model(data, n_test, cfg) 
-    
-    return score
+# create a set of sarima configs to try
+def sarima_configs(seasonal=[0]):
+    models = list()
+    # define config lists
+    p_params = [0, 1, 2]
+    d_params = [0, 1]
+    q_params = [0, 1, 2]
+    t_params = ['n','c','t','ct']
+    P_params = [0, 1, 2]
+    D_params = [0, 1]
+    Q_params = [0, 1, 2]
+    m_params = seasonal
+    # create config instances
+    for p in p_params:
+        for d in d_params:
+            for q in q_params:
+                for t in t_params:
+                    for P in P_params:
+                        for D in D_params:
+                            for Q in Q_params:
+                                for m in m_params:
+                                    cfg = [(p,d,q), (P,D,Q,m), t]
+                                    models.append(cfg)
+    return models
 
 
 if __name__ == '__main__':
-    # define dataset
-    from datetime import datetime
-    parser=lambda x: datetime.fromtimestamp(int(x)/1000)
-    series = read_csv('cpmyard.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-
-    data = series.values
-    
-    print(data[:10])
     # data split
-    n_test = int(len(data) * 0.34)
-    print(n_test)
-    # model configs
+    n_test = int(1000)
+    
     cfg_list = sarima_configs()
     # grid search
-    scores = grid_search(data, cfg_list, n_test, parallel=False)
-    print('done')
-    # list top 3 configs
-    for cfg, error in scores[:3]:
+    result_list = []
+    url="http://193.122.126.51:8081/cpmyard.cvs"
+    for cfg in cfg_list:
+        result = grid_search_async.grid_search.delay(url, cfg, n_test)
+        result_list.append(result)
+
+    for result in result_list:
+        (cfg, error) = result.get()
         print(cfg, error)
+    
